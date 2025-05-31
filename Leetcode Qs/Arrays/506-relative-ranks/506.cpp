c67ffb882052b9b3ca6b7fd357012531dd3720d6
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-31 09:17:47 +0530

    Move all Array Qs to dedicated directory

---
+++ Leetcode Qs/Arrays/506-relative-ranks/506.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+public:
+    std::vector<std::string> findRelativeRanks(std::vector<int>& score) {
+        
+        
+        
+    }
+};
